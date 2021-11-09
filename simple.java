d025b39fa35c75898afe2e0da899a82c58a020c7
Dineshece2020 <87377390+Dineshece2020@users.noreply.github.com>
2021-11-09 03:45:59 -0800

    Update simple.java

--- simple.java
+++ simple.java
@@ -1,7 +1,8 @@
 class Simple{  
     public static void main(String args[]){  
      System.out.println("Hello Java"); 
-        System.out.println("Hello broo"); 
+     System.out.println("Hello broo"); 
+        
         
     }  
 }  
